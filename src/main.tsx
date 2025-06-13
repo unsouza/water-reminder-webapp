cfda8d497f36ad5d4226f4b579fd43451eee15ea
Thiago Souza <thiagocss123@gmail.com>
2025-06-13 10:29:36 -0300

    improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import './index.css'
 
 import UnauthRoutes from './routes/guest';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="App h-screen">
       <HashRouter>
-          <Routes>
-            {UnauthRoutes}
-          </Routes>
+        <Routes>
+          <Route path="*" element={<Navigate to="/register-user" replace />} />,
+          {UnauthRoutes}
+        </Routes>
       </HashRouter>
     </div>
   </React.StrictMode>,
